ccfecd550c39617b92ad2b270ec3396344e59e0e
zephyr0815 <ljh5567@gmail.com>
2025-05-20 04:57:46 +0800

    Delete node_modules directory

--- node_modules/fast-printf/dist/src/createPrintf.d.ts
+++
@@ -1,8 +0,0 @@
-import type { PlaceholderToken } from './types';
-type FormatUnboundExpression = (subject: string, token: PlaceholderToken, boundValues: any[]) => string;
-type Configuration = {
-    formatUnboundExpression: FormatUnboundExpression;
-};
-type Printf = (subject: string, ...boundValues: any[]) => string;
-export declare const createPrintf: (configuration?: Configuration) => Printf;
-export {};
